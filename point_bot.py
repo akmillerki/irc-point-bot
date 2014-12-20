beb66cb397cc2d41d26a1f5a9ae63d62ef80462d
Ricardo Matsui <ricmatsui@gmail.com>
2014-12-20 14:22:13 -0800

    Added stats, description, removal of points

--- point_bot.py
+++ point_bot.py
@@ -6,7 +6,7 @@ yaml.add_representer(collections.defaultdict, yaml.representer.Representer.repre
 
 class PointBot(irc.bot.SingleServerIRCBot):
 
-    def __init__(self, channel, record_filename, prefix='!point', nickname='point_bot', server='irc.freenode.net', port=6667):
+    def __init__(self, channel, record_filename, prefix='!points', nickname='point_bot', server='irc.freenode.net', port=6667):
         super(PointBot, self).__init__([(server, port)], nickname, nickname)
         self.channel = channel
         self.prefix = prefix
@@ -16,7 +16,7 @@ class PointBot(irc.bot.SingleServerIRCBot):
     def load_points(self):
         try:
             with open(self.record_filename, 'r') as record_file:
-                self.record = yaml.safe_load(record_file)
+                self.record = yaml.load(record_file)
                 if not self.record:
                     raise ValueError
         except (IOError, ValueError):
@@ -36,18 +36,64 @@ class PointBot(irc.bot.SingleServerIRCBot):
     def on_pubmsg(self, connection, event):
         message = event.arguments[0]
         if message.startswith(self.prefix):
-            point_message = message[len(self.prefix):]
-            self.process_point_message(point_message, connection, event)
+            point_message = message[len(self.prefix):].strip()
+            if point_message.startswith('stats'):
+                self.send_point_stats(point_message, connection, event)
+            elif point_message.startswith('remove'):
+                self.process_remove_message(point_message, connection, event)
+            elif point_message:
+                self.process_point_message(point_message, connection, event)
+            else:
+                self.send_description(connection, event)
+
+    def send_description(self, connection, event):
+        connection.privmsg(self.channel, 'Try {} [(<points> <nick>) | (stats [<nick>]) | (remove <nick>)]'.format(self.prefix))
+
+    def send_point_stats(self, message, connection, event):
+        arguments = message.split()
+        try:
+            target = arguments[1]
+        except IndexError:
+            target = None
+        count_to_show = 20
+        if not target:
+            connection.privmsg(self.channel, 'Top {}'.format(count_to_show))
+        top_nicks = sorted(((v,k) for k,v in self.record['points'].iteritems()), reverse=True)
+        matching_nicks = [(value, nick) for value, nick in top_nicks
+                if target is None or nick.startswith(target)]
+        for value, nick in matching_nicks[:count_to_show]:
+            connection.privmsg(self.channel, '{} - {}'.format(value, nick))
+
+    def process_remove_message(self, message, connection, event):
+        try:
+            source = event.source.nick
+            target = message.split()[1]
+            if source == target:
+                connection.privmsg(self.channel, 'You can\'t remove yourself!')
+            else:
+                self.remove_points(source, target)
+                connection.privmsg(self.channel, '{} removed record of points for {}'.format(source, target))
+        except IndexError:
+            connection.privmsg(self.channel, 'Use the form: {} remove <nick>'.format(self.prefix))
+        except KeyError:
+            connection.privmsg(self.channel, 'No points recorded for {}!'.format(target))
 
     def process_point_message(self, message, connection, event):
         try:
             source = event.source.nick
-            value_string, target = message.split()
+            target, value_string = message.split()
             value = int(value_string)
-            self.give_points(source, value, target)
-            connection.privmsg(self.channel, '{} gave {} to {}'.format(source, value, target))
+            if source == target:
+                connection.privmsg(self.channel, 'You can\'t give yourself points!')
+            else:
+                self.give_points(source, value, target)
+                connection.privmsg(self.channel, '{} gave {} to {}'.format(source, value, target))
         except ValueError:
-            connection.privmsg(self.channel, 'Use the format: {} <value> <nick>'.format(self.prefix))
+            connection.privmsg(self.channel, 'Use the format: {} <nick> <value>'.format(self.prefix))
+
+    def remove_points(self, source, target):
+        del self.record['points'][target]
+        self.save_points()
 
     def give_points(self, source, value, target):
         self.record['points'][target] += value
