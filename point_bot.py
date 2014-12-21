39e10c1ed02083fbb440ed953dc43b95d8e40666
Ricardo Matsui <ricmatsui@gmail.com>
2014-12-20 16:41:16 -0800

    Added logging

--- point_bot.py
+++ point_bot.py
@@ -3,8 +3,12 @@ import collections
 import dotenv
 import irc.bot
 import itertools
+import logging
 import os
 import yaml
+
+logger = logging.getLogger('point_bot')
+logger.addHandler(logging.NullHandler())
 
 class PointBot(irc.bot.SingleServerIRCBot):
 
@@ -14,6 +18,7 @@ class PointBot(irc.bot.SingleServerIRCBot):
     TOP_COUNT = 20
     TOP_MESSAGE_FORMAT = 'Top {count}'
     TOP_ENTRY_FORMAT = '{value} - {nick}'
+    NO_ENTRIES_MESSAGE = 'No recorded points'
     SELF_REMOVAL_MESSAGES = [
         'You can\'t remove yourself!',
         'Why should I let you do that?',
@@ -56,6 +61,7 @@ class PointBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, record_filename, prefix='!points',
             nickname='point_bot', server='irc.freenode.net', port=6667):
         super(PointBot, self).__init__([(server, port)], nickname, nickname)
+        logger.debug('Starting')
         self.channel = channel
         self.prefix = prefix
         self.record_filename = record_filename
@@ -70,21 +76,27 @@ class PointBot(irc.bot.SingleServerIRCBot):
                 self.record = yaml.load(record_file)
                 if not self.record:
                     raise ValueError
+                logger.info('Points loaded from file')
         except (IOError, ValueError):
+            logger.warning('No points loaded from file')
             self.record = {}
         self.record['points'] = self.record.get('points', collections.defaultdict(int))
 
     def save_points(self):
         with open(self.record_filename, 'w') as record_file:
             yaml.dump(self.record, record_file)
+            logger.info('Points saved')
 
     def on_nicknameinuse(self, connection, event):
         connection.nick(connection.get_nickname() + '_')
+        logger.warning('Changing nickname')
 
     def on_welcome(self, connection, event):
+        logger.info('Connected')
         connection.join(self.channel)
 
     def on_pubmsg(self, connection, event):
+        logger.debug('Message received')
         message = event.arguments[0]
         if message.startswith(self.prefix):
             point_message = message[len(self.prefix):].strip()
@@ -98,6 +110,7 @@ class PointBot(irc.bot.SingleServerIRCBot):
                 self.send_description(connection, event)
 
     def send_description(self, connection, event):
+        logger.debug('Sending description')
         connection.privmsg(self.channel, self.HELP_MESSAGE_FORMAT.format(
                 prefix=self.prefix))
 
@@ -107,17 +120,20 @@ class PointBot(irc.bot.SingleServerIRCBot):
             target = arguments[1]
         except IndexError:
             target = None
-        if not target:
-            connection.privmsg(self.channel,
-                    self.TOP_MESSAGE_FORMAT.format(count=self.TOP_COUNT))
-
         top_nicks = sorted(((v,k) for k,v in self.record['points'].iteritems()),
                 reverse=True)
         matching_nicks = [(value, nick) for value, nick in top_nicks
                 if target is None or nick.startswith(target)]
-        for value, nick in matching_nicks[:self.TOP_COUNT]:
-            connection.privmsg(self.channel,
-                    self.TOP_ENTRY_FORMAT.format(value=value, nick=nick))
+        if matching_nicks:
+            if not target:
+                connection.privmsg(self.channel,
+                        self.TOP_MESSAGE_FORMAT.format(count=self.TOP_COUNT))
+            for value, nick in matching_nicks[:self.TOP_COUNT]:
+                connection.privmsg(self.channel,
+                        self.TOP_ENTRY_FORMAT.format(value=value, nick=nick))
+        else:
+            connection.privmsg(self.channel, self.NO_ENTRIES_MESSAGE)
+        logger.info('Sent stats')
 
     def process_remove_message(self, message, connection, event):
         try:
@@ -127,6 +143,7 @@ class PointBot(irc.bot.SingleServerIRCBot):
                 connection.privmsg(self.channel, next(self.self_removal_messages))
             else:
                 self.remove_points(source, target)
+                logger.info('Removed points for {}'.format(target))
                 connection.privmsg(self.channel,
                         self.REMOVAL_MESSAGE_FORMAT.format(source=source,
                                                            target=target))
@@ -145,6 +162,7 @@ class PointBot(irc.bot.SingleServerIRCBot):
             if source == target:
                 connection.privmsg(self.channel, next(self_points_messages))
             else:
+                logger.info('Giving points {} -> {}: {}'.format(source, target, value))
                 self.give_points(source, value, target)
                 plural_value = '' if value == 1 else 's'
                 points_message = next(self.points_messages)[0 if value >= 0 else 1]
@@ -166,6 +184,10 @@ class PointBot(irc.bot.SingleServerIRCBot):
         self.save_points()
 
 def main():
+    import logging.config
+    with open(os.path.join(os.path.dirname(__file__), 'logging.yml'), 'r') as config:
+        logging.config.dictConfig(yaml.load(config))
+
     dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
     parser = argparse.ArgumentParser(description='IRC Bot for Keeping Score')
 
